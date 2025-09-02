82720e6c6c0bfd07a205ae6dab850c99d875b0bf
hqbao202 <74838783+Tres0rsHoang@users.noreply.github.com>
2025-09-03 05:22:05 +0700

    Add font loading

--- next-app/src/components/data_preload_provider.tsx
+++ next-app/src/components/data_preload_provider.tsx
@@ -7,10 +7,16 @@ import { useAuthStore } from "@/store/auth.store";
 type Props = {
   children: React.ReactNode;
   images: string[];
+  fonts: string[];
 };
 
-export default function DataPreloadProvider({ children, images }: Props) {
-  const [loading, setLoading] = useState(true);
+export default function DataPreloadProvider({
+  children,
+  images,
+  fonts,
+}: Props) {
+  const [imageLoading, setImageLoading] = useState(true);
+  const [fontLoading, setFontLoading] = useState(true);
   const { isRefreshing: accessTokenRefreshing, ensureToken } = useAuthStore();
 
   useEffect(() => {
@@ -35,16 +41,56 @@ export default function DataPreloadProvider({ children, images }: Props) {
     });
   };
 
+  const preloadFont = (
+    name: string,
+    url: string,
+    retries = 3,
+  ): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const attempt = (remain: number) => {
+        const font = new FontFace(name, `url(${url}?t=${Date.now()})`);
+
+        font
+          .load()
+          .then((loadedFace) => {
+            // Add font vào document
+            document.fonts.add(loadedFace);
+            resolve();
+          })
+          .catch((err) => {
+            if (remain > 0) {
+              attempt(remain - 1);
+            } else {
+              reject(new Error(`Failed to load font: ${url}, error: ${err}`));
+            }
+          });
+      };
+
+      attempt(retries);
+    });
+  };
+
   useEffect(() => {
     const promises = images.map((src) =>
       preloadImage(src, 3).catch((err) => {
         console.error(err);
       }),
     );
-    Promise.all(promises).then(() => setLoading(false));
+    Promise.all(promises).then(() => setImageLoading(false));
   }, [images]);
 
-  return loading || accessTokenRefreshing ? (
+  useEffect(() => {
+    const promises = fonts.map((src) =>
+      preloadFont("My Fonts", src, 3).catch((err) => {
+        console.error(err);
+      }),
+    );
+    Promise.all(promises).then(() => setFontLoading(false));
+  }, [fonts]);
+
+  useEffect(() => {});
+
+  return imageLoading || accessTokenRefreshing || fontLoading ? (
     <div className="fixed inset-0 flex items-center justify-center bg-(--background) text-white z-50">
       <Loading isShow={true} />
     </div>
