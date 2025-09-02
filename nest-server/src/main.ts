5e2404007178812273ead9d8e4c4e8181056ec18
hqbao202 <74838783+Tres0rsHoang@users.noreply.github.com>
2025-09-02 07:14:22 +0700

    Fix CORS

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -8,18 +8,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   const { httpAdapter } = app.get(HttpAdapterHost);
+
   app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
+
   app.enableCors({
     origin: [process.env.PUBLIC_APP_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
   app.setGlobalPrefix('api');
   await app.listen(process.env.SERVER_PORT ?? 7110);
 }
