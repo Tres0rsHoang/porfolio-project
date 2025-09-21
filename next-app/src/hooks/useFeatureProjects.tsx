7ac7181909c9f5037b40e6d3344e0945f1868b61
hqbao202 <74838783+Tres0rsHoang@users.noreply.github.com>
2025-09-21 11:00:36 +0700

    Fix bug in fetch feature project

--- next-app/src/hooks/useFeatureProjects.tsx
+++ next-app/src/hooks/useFeatureProjects.tsx
@@ -11,19 +11,20 @@ async function fetchFeatureProject() {
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/project/feature`,
   );
   const resBody = await res.json();
-  if (!Array.isArray(resBody) || !res.ok) {
+
+  if (!Array.isArray(resBody.data) || !res.ok) {
     throw new Error("Invalid project/feature responsive");
   }
 
-  const formatedProject: Project[] = resBody.map((value: RawProject) => {
+  const formatedProject: Project[] = resBody.data.map((value: RawProject) => {
     const projectInfo: Project = {
       id: value.id,
       name: value.name,
       description: value.description,
       role: value.role,
-      responsibilities: value.responsibilities.map((value) => value),
       startAt: new Date(value.startAt),
       endAt: new Date(value.endAt),
+      responsibilities: value.responsibilities.map((value) => value),
       frameworks: value.frameworks.map((value) => value.name),
       languages: value.languages.map((value) => value.name),
       types: value.types.map(
@@ -46,6 +47,7 @@ export default function useFeatureProject() {
     queryKey: ["featureProject"],
     queryFn: fetchFeatureProject,
   });
+
   let data: Project[] = [];
   if (result) data = result;
   return { data, isLoading, error, refetch };
